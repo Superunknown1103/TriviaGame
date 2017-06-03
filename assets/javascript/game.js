10bc384d1b384cd1c71015a5478e25db0bf5be36
Bruce Tsvil <superunknown1103@gmail.com>
2017-06-03 18:49:57 -0500

    javascript added

---
+++ assets/javascript/game.js
@@ -0,0 +1,22 @@
+var questions = [
+	{
+		question: "When was the first Ford Mustang released?",
+		answer: ["1962","1970", "1964", "1969"],
+		correctAnswer: "1964"
+	},
+	{
+		question: "Which classic car holds the world record for the most miles ever driven on one engine?",
+		answer: ["1966 Volvo P1800","1990 Honda Accord", "1996 Dodge 2500", "1989 Saab SPG"],
+		correctAnswer: "1966 Volvo P1800"
+
+	}
+];
+
+
+$('#start').on('click', function() {
+	startGame();
+});
+
+function startGame(); {
+	
+}
